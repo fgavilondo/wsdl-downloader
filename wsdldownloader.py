d498d43700d834a92325fce79ca6b8ba163912cf
Fabio Gavilondo <fabio.gavilondo@gmail.com>
2017-09-04 18:12:57 +1000

    Minor improvements re WSDL name calculation

--- wsdldownloader.py
+++ wsdldownloader.py
@@ -3,9 +3,13 @@ from __future__ import print_function
 
 import os
 import sys
-import urllib2
-import urlparse
+import urllib2  # Python 2 only
+import urlparse  # Python 2 only
 from xml.dom import minidom
+
+INDENT_LEVEL = 2
+
+DOWNLOADED_URLS = []
 
 
 def write_file(filename, content):
@@ -15,41 +19,57 @@ def write_file(filename, content):
 
 
 def xml_to_pretty_string(xml):
-    return xml.toprettyxml(indent='    ')
+    return xml.toprettyxml(indent=' ' * INDENT_LEVEL)
 
 
-def download_wsdl_imports(wsdl_doc, output_dir):
+def is_already_downloaded(url):
+    return url in DOWNLOADED_URLS
+
+
+def mark_as_downloaded(url):
+    DOWNLOADED_URLS.append(url)
+
+
+def get_url_query_params_as_dict(url):
+    return dict(urlparse.parse_qsl(urlparse.urlsplit(url).query))
+
+
+def download_wsdl_imports(wsdl_url, wsdl_doc, output_dir):
     wsdl_import_elems = wsdl_doc.getElementsByTagName('wsdl:import')
     if wsdl_import_elems:
-        print('Fetching child WSDLs for WSDL', get_wsdl_name(wsdl_doc))
+        print('Fetching child WSDLs for: ', get_wsdl_name(wsdl_url, wsdl_doc))
     for elem in wsdl_import_elems:
-        wsdl_url = elem.attributes['location'].value
-        url_query_params = dict(urlparse.parse_qsl(urlparse.urlsplit(wsdl_url).query))
+        child_wsdl_url = elem.attributes['location'].value
+        if is_already_downloaded(child_wsdl_url):
+            print(child_wsdl_url, 'already downloaded - ignoring.')
+            continue
+        child_wsdl_doc = read_xml_from_url(child_wsdl_url)
+        download_wsdl_imports(child_wsdl_url, child_wsdl_doc, output_dir)
+        download_xsd_imports(child_wsdl_url, child_wsdl_doc, output_dir)
+        url_query_params = get_url_query_params_as_dict(child_wsdl_url)
         child_wsdl_filename = url_query_params['wsdl'] + '.wsdl'
         path = os.path.join(output_dir, child_wsdl_filename)
-        if not os.path.isfile(path):
-            # only download it if not already there
-            child_wsdl_doc = read_xml_from_url(wsdl_url)
-            download_xsd_imports(child_wsdl_doc, output_dir)
-            download_wsdl_imports(child_wsdl_doc, output_dir)
-            write_file(path, xml_to_pretty_string(child_wsdl_doc))
+        write_file(path, xml_to_pretty_string(child_wsdl_doc))
         elem.attributes['location'].value = child_wsdl_filename
+        mark_as_downloaded(child_wsdl_url)
 
 
-def download_xsd_imports(wsdl_doc, output_dir):
+def download_xsd_imports(wsdl_url, wsdl_doc, output_dir):
     xsd_import_elems = wsdl_doc.getElementsByTagName('xsd:import')
     if xsd_import_elems:
-        print('Fetching XSD schemas for WSDL', get_wsdl_name(wsdl_doc))
+        print('Fetching XSD schemas for: ', get_wsdl_name(wsdl_url, wsdl_doc))
     for elem in xsd_import_elems:
         xsd_url = elem.attributes['schemaLocation'].value
-        url_query_params = dict(urlparse.parse_qsl(urlparse.urlsplit(xsd_url).query))
+        if is_already_downloaded(xsd_url):
+            print('Ignoring', xsd_url, ': already downloaded')
+            continue
+        xsd_doc = read_xml_from_url(xsd_url)
+        url_query_params = get_url_query_params_as_dict(xsd_url)
         xsd_filename = url_query_params['xsd'] + '.xsd'
         path = os.path.join(output_dir, xsd_filename)
-        if not os.path.isfile(path):
-            # only download it if not already there
-            xsd_doc = read_xml_from_url(xsd_url)
-            write_file(path, xml_to_pretty_string(xsd_doc))
+        write_file(path, xml_to_pretty_string(xsd_doc))
         elem.attributes['schemaLocation'].value = xsd_filename
+        mark_as_downloaded(xsd_url)
 
 
 def read_xml_from_url(url):
@@ -62,26 +82,26 @@ def ensure_dir_exists(path):
         os.makedirs(path)
 
 
-def get_wsdl_name(wsdl_doc):
+def get_wsdl_name(wsdl_url, wsdl_doc):
     wsdl_definitions_elem = wsdl_doc.getElementsByTagName('wsdl:definitions')[0]
     attrib = wsdl_definitions_elem.attributes.get('name')
-    if attrib:
+    if attrib and attrib.value:
         return attrib.value
     else:
-        attrib = wsdl_definitions_elem.attributes.get('targetNamespace')
-        return 'child wsdl for target namespace ' + attrib.value
+        url_query_params = get_url_query_params_as_dict(wsdl_url)
+        return url_query_params['wsdl']
 
 
-def get_wsdl_file_name(wsdl_doc):
-    return get_wsdl_name(wsdl_doc) + '.wsdl'
+def get_wsdl_file_name(wsdl_url, wsdl_doc):
+    return get_wsdl_name(wsdl_url, wsdl_doc) + '.wsdl'
 
 
 def process(wsdl_url, output_dir):
     ensure_dir_exists(output_dir)
     wsdl_doc = read_xml_from_url(wsdl_url)
-    download_wsdl_imports(wsdl_doc, output_dir)
-    download_xsd_imports(wsdl_doc, output_dir)
-    wsdl_filename = get_wsdl_file_name(wsdl_doc)
+    download_wsdl_imports(wsdl_url, wsdl_doc, output_dir)
+    download_xsd_imports(wsdl_url, wsdl_doc, output_dir)
+    wsdl_filename = get_wsdl_file_name(wsdl_url, wsdl_doc)
     write_file(os.path.join(output_dir, wsdl_filename), xml_to_pretty_string(wsdl_doc))
 
 
@@ -102,6 +122,7 @@ def usage():
 
 
 def main():
+    wsdl_url = ''
     try:
         wsdl_url = sys.argv[1]
     except IndexError:
@@ -118,3 +139,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
