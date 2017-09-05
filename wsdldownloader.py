f9b9d35edc45d56e3db6be85776d8ffd473a0684
Fabio Gavilondo <fabio.gavilondo@gmail.com>
2017-09-05 21:24:05 +1000

    Build the WSDL file name from the URL path if the WSDL definition does not contain a name

--- wsdldownloader.py
+++ wsdldownloader.py
@@ -34,18 +34,18 @@ def get_url_query_params_as_dict(url):
     return dict(urlparse.parse_qsl(urlparse.urlsplit(url).query))
 
 
-def download_wsdl_imports(wsdl_url, wsdl_doc, output_dir):
+def download_wsdl_imports(wsdl_doc, output_dir):
     wsdl_import_elems = wsdl_doc.getElementsByTagName('wsdl:import')
     if wsdl_import_elems:
-        print('Fetching child WSDLs for: ', get_wsdl_name(wsdl_url, wsdl_doc))
+        print('Fetching child WSDLs')
     for elem in wsdl_import_elems:
         child_wsdl_url = elem.attributes['location'].value
         if is_already_downloaded(child_wsdl_url):
             print(child_wsdl_url, 'already downloaded - ignoring.')
             continue
         child_wsdl_doc = read_xml_from_url(child_wsdl_url)
-        download_wsdl_imports(child_wsdl_url, child_wsdl_doc, output_dir)
-        download_xsd_imports(child_wsdl_url, child_wsdl_doc, output_dir)
+        download_wsdl_imports(child_wsdl_doc, output_dir)
+        download_xsd_imports(child_wsdl_doc, output_dir)
         url_query_params = get_url_query_params_as_dict(child_wsdl_url)
         child_wsdl_filename = url_query_params['wsdl'] + '.wsdl'
         path = os.path.join(output_dir, child_wsdl_filename)
@@ -54,10 +54,10 @@ def download_wsdl_imports(wsdl_url, wsdl_doc, output_dir):
         mark_as_downloaded(child_wsdl_url)
 
 
-def download_xsd_imports(wsdl_url, wsdl_doc, output_dir):
+def download_xsd_imports(wsdl_doc, output_dir):
     xsd_import_elems = wsdl_doc.getElementsByTagName('xsd:import')
     if xsd_import_elems:
-        print('Fetching XSD schemas for: ', get_wsdl_name(wsdl_url, wsdl_doc))
+        print('Fetching XSD schemas')
     for elem in xsd_import_elems:
         xsd_url = elem.attributes['schemaLocation'].value
         if is_already_downloaded(xsd_url):
@@ -74,7 +74,8 @@ def download_xsd_imports(wsdl_url, wsdl_doc, output_dir):
 
 def read_xml_from_url(url):
     print('Reading', url)
-    return minidom.parseString(urllib2.urlopen(url).read())
+    text = urllib2.urlopen(url).read()
+    return minidom.parseString(text)
 
 
 def ensure_dir_exists(path):
@@ -82,26 +83,27 @@ def ensure_dir_exists(path):
         os.makedirs(path)
 
 
-def get_wsdl_name(wsdl_url, wsdl_doc):
+def get_main_wsdl_name(wsdl_url, wsdl_doc):
     wsdl_definitions_elem = wsdl_doc.getElementsByTagName('wsdl:definitions')[0]
     attrib = wsdl_definitions_elem.attributes.get('name')
     if attrib and attrib.value:
         return attrib.value
     else:
-        url_query_params = get_url_query_params_as_dict(wsdl_url)
-        return url_query_params['wsdl']
+        path = urlparse.urlsplit(wsdl_url).path
+        path = path[1:]  # remove the leading '/'
+        return path.replace('/', '-')  # replace all other '/' with dashes
 
 
-def get_wsdl_file_name(wsdl_url, wsdl_doc):
-    return get_wsdl_name(wsdl_url, wsdl_doc) + '.wsdl'
+def get_main_wsdl_file_name(wsdl_url, wsdl_doc):
+    return get_main_wsdl_name(wsdl_url, wsdl_doc) + '.wsdl'
 
 
 def process(wsdl_url, output_dir):
     ensure_dir_exists(output_dir)
     wsdl_doc = read_xml_from_url(wsdl_url)
-    download_wsdl_imports(wsdl_url, wsdl_doc, output_dir)
-    download_xsd_imports(wsdl_url, wsdl_doc, output_dir)
-    wsdl_filename = get_wsdl_file_name(wsdl_url, wsdl_doc)
+    download_wsdl_imports(wsdl_doc, output_dir)
+    download_xsd_imports(wsdl_doc, output_dir)
+    wsdl_filename = get_main_wsdl_file_name(wsdl_url, wsdl_doc)
     write_file(os.path.join(output_dir, wsdl_filename), xml_to_pretty_string(wsdl_doc))
 
 
@@ -139,4 +141,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
